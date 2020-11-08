e44eacc484a1aeb411cc8e1f6c4bf857226a3d89
KuroShure <rekuiemu9@gmail.com>
2020-11-08 14:21:05 +0900

    シーン切り替えを追加したバージョン　最初の投稿のやつに俺のシーン切り替えを投稿したやつ

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -2,21 +2,26 @@
 #include "DirectXCommon.h"
 #include "Audio.h"
 #include "GameScene.h"
+#include "SceneManager.h"
+#include "BaseScene.h"
+#include "TitleScene.h"
+
 
 // Windowsアプリでのエントリーポイント(main関数)
-int WINAPI WinMain(HINSTANCE,HINSTANCE,LPSTR,int)
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
 	// 汎用機能
 	WinApp* win = nullptr;
 	DirectXCommon* dxCommon = nullptr;
-	Input* input = nullptr;	
+	Input* input = nullptr;
 	Audio* audio = nullptr;
-	GameScene* gameScene = nullptr;
+	//GameScene* gameScene = nullptr;
+	SceneManager* sceneManager = nullptr;
 
 	// ゲームウィンドウの作成
 	win = new WinApp();
 	win->CreateGameWindow();
-		
+
 	//DirectX初期化処理
 	dxCommon = new DirectXCommon();
 	dxCommon->Initialize(win);
@@ -39,7 +44,7 @@ int WINAPI WinMain(HINSTANCE,HINSTANCE,LPSTR,int)
 		assert(0);
 		return 1;
 	}
-	
+
 	// 3Dオブジェクト静的初期化
 	if (!Object3d::StaticInitialize(dxCommon->GetDevice(), WinApp::window_width, WinApp::window_height)) {
 		assert(0);
@@ -61,29 +66,36 @@ int WINAPI WinMain(HINSTANCE,HINSTANCE,LPSTR,int)
 #pragma endregion
 
 	// ゲームシーンの初期化
-	gameScene = new GameScene();
-	gameScene->Initialize(dxCommon, input, audio);
-	
+	//gameScene = new GameScene();
+	//gameScene->Initialize(dxCommon, input, audio);
+	sceneManager = new SceneManager();//追加ーーーー
+	sceneManager->Init(dxCommon, input, audio);
+
+	sceneManager->AddScene(SCENE::TITLE, new TitleScene());
+	sceneManager->AddScene(SCENE::GAME, new GameScene());
+	sceneManager->ChangeScene(GAME);//ここでシーン切り替え
+
 	// メインループ
 	while (true)
 	{
 		// メッセージ処理
-		if (win->ProcessMessage()) {	break; }
+		if (win->ProcessMessage()) { break; }
 
 		// 入力関連の毎フレーム処理
 		input->Update();
 		// ゲームシーンの毎フレーム処理
-		gameScene->Update();
+		sceneManager->Update();
 
 		// 描画開始
 		dxCommon->PreDraw();
 		// ゲームシーンの描画
-		gameScene->Draw();
+		sceneManager->Draw();
 		// 描画終了
 		dxCommon->PostDraw();
 	}
 	// 各種解放
-	safe_delete(gameScene);
+	safe_delete(sceneManager);
+	//safe_delete(gameScene);
 	safe_delete(audio);
 	safe_delete(input);
 	safe_delete(dxCommon);
